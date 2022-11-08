528edfafbfa5e4fc3cf262975a84cb778d39553e
Masaharu KATO <fudai.mk@gmail.com>
2022-11-08 20:50:14 +0000

    make basic frontend and backend

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <input type='text' name='text' v-model='text'>
+    <br>
+    <button @click='post'>post</button>
+  </div>
+
+</template>
+
+<script>
+import Methods from '@/api/methods'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      text: ''
+    }
+  },
+  methods: {
+    async post() {
+      let element = { text: this.text }
+      let response = await Methods.testPosting(element)
+      console.log(response.data.message)
+    }
+  }
+}
+</script>
